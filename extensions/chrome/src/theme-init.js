7f2644a977f18cabea00856bbea94bde8dcab997
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-11-11 18:42:23 +0300

    Refactor theme-init.js and tracker.js fix linters

--- extensions/chrome/src/theme-init.js
+++ extensions/chrome/src/theme-init.js
@@ -5,7 +5,12 @@
 import ThemeInitializer from './managers/theme/ThemeInitializer.js';
 import CONFIG from './config/config.js';
 
+try {
     const themeInitializer = new ThemeInitializer({
         enableLogging: CONFIG.THEME.INIT.ENABLE_LOGGING
     });
     themeInitializer.init();
+} catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('[ThemeInit] Критическая ошибка инициализации темы:', error);
+}
